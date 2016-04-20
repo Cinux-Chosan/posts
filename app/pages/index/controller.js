3d82a78b7be14f831f4ecec1546dbe41e6b99e56
Chosan <179817004@qq.com>
2016-04-21 00:16:05 +0800

    index reference to http://expo.bootcss.com/slimfaq/

--- app/pages/index/controller.js
+++ app/pages/index/controller.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+    actions: {
+        didInsert() {
+            let $navbar = $('ul.nav.navbar-nav');
+            window.animateCss($navbar, 'swing');
+
+        }
+    }
 });
--- app/pages/index/route.js
+++ app/pages/index/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model(){
+    model() {
         window.myGetJson('http://rap.taobao.org/mockjsdata/2909/posts/index?').then(data => {
             console.log(data);
         });
